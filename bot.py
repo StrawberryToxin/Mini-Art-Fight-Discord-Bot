af803f1c3e88da766aebe8e0fbe4c313cef04bb1
StrawberryToxin <39465687+StrawberryToxin@users.noreply.github.com>
2018-07-06 17:29:21 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -317,9 +317,7 @@ async def attackpoints(ctx):
 @bot.command(pass_context=True)
 async def assignteam(ctx):
     user = ctx.message.author
-    if "TEAM WHITE" in [y.name.upper() for y in ctx.message.author.roles]:
-        await bot.say("I'm sorry, you cannot use this command right now!")
-    elif "TEAM BLACK" in [y.name.upper() for y in ctx.message.author.roles]:
+    if "TEAM WHITE" in [y.name.upper() for y in ctx.message.author.roles] or "TEAM BLACK" in [y.name.upper() for y in ctx.message.author.roles]:
         await bot.say("I'm sorry, you cannot use this command right now!")
     else:
         role = ((discord.utils.get(user.server.roles, name="Team Black")), (discord.utils.get(user.server.roles, name="Team White")))
